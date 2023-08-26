acd3f2c96f4bdfadfc91701df153bc6cdb097214
Numan Abubakar <bhalonoman@gmail.com>
2023-08-26 22:12:24 +0500

    task 2 and 3 comp

--- assignment02/assignment02.js
+++ assignment02/assignment02.js
@@ -10,3 +10,17 @@ function sumOfEvenNumbers(n) {
 const n = 5;
 const sum = sumOfEvenNumbers(n);
 console.log(`The sum of the first ${n} even numbers is: ${sum}`);
+// 2. Implement a program that uses a for loop to iterate through an array of numbers and print only the even numbers.
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+for (var i = 0; i < numbers.length; i++) {
+    if (numbers[i] % 2 === 0) {
+        console.log(numbers[i]);
+    }
+}
+// 3. Implement a program that uses a loop to iterate through an array of numbers and remove all the even numbers, leaving only the odd numbers.
+for (var i = 0; i < numbers.length; i++) {
+    if (numbers[i] % 2 === 0) {
+        numbers.splice(i, 1);
+    }
+}
+console.log(numbers);
